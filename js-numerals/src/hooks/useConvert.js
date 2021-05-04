1c7bdce0c4d237b96a33153fbc0082621c0c08b0
VIctor <victor.vargas@etalytics.de>
2021-05-04 09:35:29 -0300

    Finished useConvert hook

--- js-numerals/src/hooks/useConvert.js
+++ js-numerals/src/hooks/useConvert.js
@@ -1,5 +1,7 @@
 import { NUMBERS_STRINGS, SUFFIXES } from "../constants";
 
+const noop = () => {};
+
 export default function useConvert(number) {
   if (number) {
     if (NUMBERS_STRINGS.hasOwnProperty(number)) {
@@ -11,6 +13,8 @@ export default function useConvert(number) {
 
     if (stringifiedNumber.length % 3 === 0) {
       const blocksOfHundreds = stringifiedNumber.match(/.{1,3}/g);
+      const blocksOfHundredsCopy = [...blocksOfHundreds];
+
       for (const [index, block] of blocksOfHundreds.entries()) {
         finalNumber.push(NUMBERS_STRINGS[parseInt(block[0])] + " ");
         finalNumber.push(SUFFIXES.HUNDRED + " and ");
@@ -28,7 +32,25 @@ export default function useConvert(number) {
             );
           }
         }
+
         if (index !== blocksOfHundreds.length - 1) {
+          switch (blocksOfHundredsCopy.length) {
+            case 2:
+              finalNumber.push(` ${SUFFIXES.THOUSAND} `);
+              break;
+            case 3:
+              finalNumber.push(` ${SUFFIXES.MILLION} `);
+              break;
+            case 4:
+              finalNumber.push(` ${SUFFIXES.BILLION} `);
+              break;
+            case 5:
+              finalNumber.push(` ${SUFFIXES.TRILLION} `);
+              break;
+            default:
+              noop();
+          }
+          blocksOfHundredsCopy.pop();
           finalNumber.push(" and ");
         }
       }
